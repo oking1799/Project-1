0f83ccbef15dac4bf5e8825291f678cbdcfb4e74
Owen King <oking1799@gmail.com>
2020-10-01 13:06:38 -0400

    Prototype

---
+++ src/jsonResponses.js
@@ -0,0 +1,72 @@
+const locations = {};
+
+const respondJSON = (request, response, status, object) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+const respondJSONMeta = (request, response, status) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
+};
+
+const addLocation = (request, response, body) => {
+  const responseJSON = {
+    message: 'A Name, Latitude and Longitude are required',
+  };
+
+  console.log(body);
+
+  if (!body.name || !body.latitude || !body.longitude) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  let responseCode = 201;
+
+  if (locations[body.name]) {   //check for update
+    responseCode = 204;
+  } else {
+    locations[body.name] = {};
+  }
+
+  locations[body.name].name = body.name
+  locations[body.name].latitude = body.latitude;
+  locations[body.name].longitude = body.longitude;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+
+  return respondJSONMeta(request, response, responseCode);
+};
+
+const getLocation = (request, response) => {
+  const responseJSON = {
+    locations,
+  };
+  return respondJSON(request, response, 200, responseJSON);
+};
+
+const getLocationMeta = (request, response) => respondJSONMeta(request, response, 200);
+
+const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found!',
+    id: 'notFound',
+  };
+  return respondJSON(request, response, 404, responseJSON);
+};
+
+const notFoundMeta = (request, response) => respondJSONMeta(request, response, 404);
+
+module.exports = {
+  notFound,
+  notFoundMeta,
+  addLocation,
+  getLocation,
+  getLocationMeta,
+
+};
